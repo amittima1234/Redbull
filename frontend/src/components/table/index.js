89a07ec08bdd2a2f9ff59528fa5bdab09d1691a0
amittima1234 <amittima1234@gmail.com>
2023-02-04 14:58:24 +0200

    feat(): adding the react-table module

--- frontend/src/components/table/index.js
+++ frontend/src/components/table/index.js
@@ -1,32 +1,73 @@
 import React from "react";
 import styles from "./index.module.css";
-import TableRow from "../tableRow"
-import TableHeader from "../tableHeader";
-
-
-const data = [{ id: "1", component: "mosh", ip: "123.123", network: "jordan" },
-{ id: "2", component: "yos", ip: "13.23", network: "lebanon" },
-{ id: "3", component: "dos", ip: "53.223", network: "iran" },
-{ id: "4", component: "ros", ip: "68.95", network: "syria" },
-{ id: "5", component: "mos", ip: "45.74", network: "egypt" }];
-
-const headers = ["component", "ip", "network"]
-
+import { useTable } from 'react-table';
 
 const Table = () => {
 
-    return <div className={styles.table}>
-        <div className={styles["table-header"]}>
-            <TableHeader data={headers} />
-        </div>
-        <div className={styles["table-data"]}>
-            {data.map(
-                (rowData) => {
-                    return <TableRow key={rowData.id} data={rowData} />
-                })}
-        </div>
-    </div>
+    const data = React.useMemo(() => [
+        { col1: 'mosh', col2: '123.123', col3: 'jordan', },
+        { col1: 'yos', col2: '13.23', col3: 'lebanon', },
+        { col1: 'dos', col2: '53.223', col3: 'iran', },
+        { col1: 'ros', col2: '68.95', col3: 'syria', },
+        { col1: 'mosh', col2: '45.74', col3: 'egypt', },], [])
 
+    const columns = React.useMemo(() => [
+        { Header: 'Component', accessor: 'col1', },
+        { Header: 'IP', accessor: 'col2', },
+        { Header: 'Network', accessor: 'col3', },], [])
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data })
+
+    return <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+        <thead>
+            {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        <th
+                            {...column.getHeaderProps()}
+                            style={{
+                                borderBottom: 'solid 3px red',
+                                background: 'aliceblue',
+                                color: 'black',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {column.render('Header')}
+                        </th>
+                    ))}
+                </tr>
+            ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                            return (
+                                <td
+                                    {...cell.getCellProps()}
+                                    style={{
+                                        padding: '10px',
+                                        border: 'solid 1px gray',
+                                        background: 'papayawhip',
+                                    }}
+                                >
+                                    {cell.render('Cell')}
+                                </td>
+                            )
+                        })}
+                    </tr>
+                )
+            })}
+        </tbody>
+    </table>
 };
 
 export default Table;
