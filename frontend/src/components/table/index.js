095bfc5e80dad061f5ec0566e5d4b01a31402945
amittima1234 <amittima1234@gmail.com>
2023-02-04 17:04:20 +0200

    feat(): basic css

--- frontend/src/components/table/index.js
+++ frontend/src/components/table/index.js
@@ -24,50 +24,36 @@ const Table = () => {
         prepareRow,
     } = useTable({ columns, data })
 
-    return <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-        <thead>
-            {headerGroups.map(headerGroup => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
+    const headerGroup = headerGroups[0];
+
+    return (
+        <table {...getTableProps()} className={styles.table}>
+            <thead className={styles["table-header"]}>
+                <tr  {...headerGroup.getHeaderGroupProps()}>
                     {headerGroup.headers.map(column => (
                         <th
                             {...column.getHeaderProps()}
-                            style={{
-                                borderBottom: 'solid 3px red',
-                                background: 'aliceblue',
-                                color: 'black',
-                                fontWeight: 'bold',
-                            }}
+                            className={styles["table-header-cell"]}
                         >
                             {column.render('Header')}
                         </th>
                     ))}
+
                 </tr>
-            ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-                prepareRow(row)
-                return (
-                    <tr {...row.getRowProps()}>
+            </thead>
+
+            <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row)
+                    return (<tr className={styles["table-data-row"]} {...row.getRowProps()}>
                         {row.cells.map(cell => {
-                            return (
-                                <td
-                                    {...cell.getCellProps()}
-                                    style={{
-                                        padding: '10px',
-                                        border: 'solid 1px gray',
-                                        background: 'papayawhip',
-                                    }}
-                                >
-                                    {cell.render('Cell')}
-                                </td>
-                            )
-                        })}
-                    </tr>
-                )
-            })}
-        </tbody>
-    </table>
+                            return (<td {...cell.getCellProps()}>
+                                {cell.render('Cell')}
+                            </td>)
+                        })}</tr>)
+                })}
+            </tbody>
+        </table>)
 };
 
 export default Table;
