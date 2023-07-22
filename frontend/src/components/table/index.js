f7d74818966a2b42fd5f326297ee4d0580daac2d
amittima1234 <amittima1234@gmail.com>
2023-07-22 21:22:39 +0300

    added xlsx library

--- frontend/src/components/table/index.js
+++ frontend/src/components/table/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import { useMemo, useState, useEffect } from "react";
 import styles from "./index.module.css";
 import { useTable } from 'react-table';
+import { read, utils, writeFile } from 'xlsx';
+
 
 const Table = () => {
 
-    const data = React.useMemo(() => [
+    const [sheet, setSheet] = useState(null);
+
+    useEffect(()  => {
+        async function fetchData() {
+            await fetch("http://localhost:5000/sheet")
+        .then(result => {
+            setSheet(result.data)
+        })
+
+    };
+    fetchData();
+    }, [sheet])
+
+    const data = useMemo(() => [
         { col1: 'mosh', col2: '123.123', col3: 'jordan', },
         { col1: 'yos', col2: '13.23', col3: 'lebanon', },
         { col1: 'dos', col2: '53.223', col3: 'iran', },
         { col1: 'ros', col2: '68.95', col3: 'syria', },
         { col1: 'mosh', col2: '45.74', col3: 'egypt', },], [])
 
-    const columns = React.useMemo(() => [
+    const columns = useMemo(() => [
         { Header: 'Component', accessor: 'col1', },
         { Header: 'IP', accessor: 'col2', },
         { Header: 'Network', accessor: 'col3', },], [])
@@ -27,33 +42,39 @@ const Table = () => {
     const headerGroup = headerGroups[0];
 
     return (
-        <table {...getTableProps()} className={styles.table}>
-            <thead className={styles["table-header"]}>
-                <tr  {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map(column => (
-                        <th
-                            {...column.getHeaderProps()}
-                            className={styles["table-header-cell"]}
-                        >
-                            {column.render('Header')}
-                        </th>
-                    ))}
+        <div>
+            <div>
+                { console.log(sheet) }
+            </div>
+            <table {...getTableProps()} className={styles.table}>
+                <thead className={styles["table-header"]}>
+                    <tr  {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th
+                                {...column.getHeaderProps()}
+                                className={styles["table-header-cell"]}
+                            >
+                                {column.render('Header')}
+                            </th>
+                        ))}
 
-                </tr>
-            </thead>
+                    </tr>
+                </thead>
 
-            <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                    prepareRow(row)
-                    return (<tr className={styles["table-data-row"]} {...row.getRowProps()}>
-                        {row.cells.map(cell => {
-                            return (<td {...cell.getCellProps()}>
-                                {cell.render('Cell')}
-                            </td>)
-                        })}</tr>)
-                })}
-            </tbody>
-        </table>)
+                <tbody {...getTableBodyProps()}>
+                    {rows.map(row => {
+                        prepareRow(row)
+                        return (<tr className={styles["table-data-row"]} {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return (<td {...cell.getCellProps()}>
+                                    {cell.render('Cell')}
+                                </td>)
+                            })}</tr>)
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 };
 
 export default Table;
